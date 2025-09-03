882f6d26545d1c81d16a508d7f23813a23a75f61
Arjun <23110040@iitgn.ac.in>
2025-09-04 02:13:04 +0530

    laptop frontend

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const nav = document.querySelector('.nav');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+const section1 = document.querySelector('#Works');
 const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 const imgTargets = document.querySelectorAll('img[data-src]');
@@ -57,27 +57,27 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 //Tabbed operation
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+// const tabs = document.querySelectorAll('.operations__tab');
+// const tabsContainer = document.querySelector('.operations__tab-container');
+// const tabsContent = document.querySelectorAll('.operations__content');
 
-tabsContainer.addEventListener('click', function (e) {
-  const clicked = e.target.closest('.operations__tab');
+// tabsContainer.addEventListener('click', function (e) {
+//   const clicked = e.target.closest('.operations__tab');
 
-  //Guard clause
-  if (!clicked) return;
+//   //Guard clause
+//   if (!clicked) return;
 
-  //Activate tabs
-  tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  clicked.classList.add('operations__tab--active');
+//   //Activate tabs
+//   tabs.forEach(t => t.classList.remove('operations__tab--active'));
+//   clicked.classList.add('operations__tab--active');
 
-  //Activate content
-  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+//   //Activate content
+//   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
 
-  document
-    .querySelector(`.operations__content--${clicked.dataset.tab}`)
-    .classList.add('operations__content--active');
-});
+//   document
+//     .querySelector(`.operations__content--${clicked.dataset.tab}`)
+//     .classList.add('operations__content--active');
+// });
 
 //Menu fade effect
 const handleHover = function (e) {
